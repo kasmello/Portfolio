6acb3db54c7485b52a7640b0ed09b4f8cbd1d1e0
kasmello <karmel.gandahusada@gmail.com>
2023-12-28 14:23:03 +0800

    first rendition of portfolio page without formatting

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <h1>Hi, I'm Karmel</h1>
+      <h3>Data Scientist/Fullstack Developer/AI Engineer</h3>
+      <p>Since 2019, I have been dedicated to studying and advancing my skills in AI and data science. My journey has been marked by a commitment to continuous learning, and exploring the latest advancements in these fields. In 2021, I started working professionally and have loved it ever since, successfully leveraging my expertise to real-world applications.</p>
+      <p>Click on the links below to see the my academic and passion projects that I have worked on during university and my free time.</p>
+      <ul>
+        <li><a href="https://kasmello.github.io/HearMe/">HearMe - an app that detects the pitch & note of your voice</a></li>
+        <li><a href="https://kasmello.github.io/MapMe/">MapMe - an app designed to plan appropriate fod/fuel stops on road trips - IN PROGRESS</a></li>
+      </ul>
+    </body>
+  );
+}
+
+export default App;
