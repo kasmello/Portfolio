d4ecbbb3dca18eab6df6a7d174e052258109bd5d
kasmello <karmel.gandahusada@gmail.com>
2023-12-30 23:08:47 +0800

    updated thumbnails to be responsive to hovering

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+import RoundedCard from './hypercard';
+import HearMeThumbnail from './hearmethumbnail.png'
+import MapMeThumbnail from './mapmethumbnail.png'
 import './App.css';
 
 function App() {
@@ -8,10 +11,20 @@ function App() {
       <h3>Data Scientist/Fullstack Developer/AI Engineer</h3>
       <p>Since 2019, I have been dedicated to studying and advancing my skills in AI and data science. My journey has been marked by a commitment to continuous learning, and exploring the latest advancements in these fields. In 2021, I started working professionally and have loved it ever since, successfully leveraging my expertise to real-world applications.</p>
       <p>Click on the links below to see the my academic and passion projects that I have worked on during university and my free time.</p>
-      <ul>
-        <li><a href="https://kasmello.github.io/HearMe/">HearMe - an app that detects the pitch & note of your voice</a></li>
-        <li><a href="https://kasmello.github.io/MapMe/">MapMe - an app designed to plan appropriate fod/fuel stops on road trips - IN PROGRESS</a></li>
-      </ul>
+      <div className="Thumbnail-collection">
+        <RoundedCard
+          imageSrc={HearMeThumbnail}
+          label="HearMe"
+          description="An app that detects the pitch & note of your voice"
+          link="https://kasmello.github.io/HearMe/"
+        />
+        <RoundedCard
+          imageSrc={MapMeThumbnail}
+          label="MapMe"
+          description="An app designed to plan appropriate fod/fuel stops on road trips - <i>IN PROGRESS</i>"
+          link="https://kasmello.github.io/MapMe/"
+        />
+      </div>
     </body>
   );
 }
---
+++ portfolio/src/hypercard.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './hypercard.css';
+
+const RoundedCard = ({ imageSrc, label, description, link }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleClick = () => {
+    window.open(link, '_blank');
+  };
+
+  return (
+    <div
+      className="rounded-card"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    >
+      <img src={imageSrc} alt={label} />
+      {/* <div className="content">
+        <h3>{isHovered ? description : label}</h3>
+      </div> */}
+    </div>
+  );
+};
+
+export default RoundedCard;
