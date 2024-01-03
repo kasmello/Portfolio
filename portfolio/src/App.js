9ed252f5cc3293ceaf656d3aca3567772e1629c1
kasmello <karmel.gandahusada@gmail.com>
2024-01-04 07:48:56 +0800

    changed vw to px styling to avoid issues

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,19 +3,19 @@ import RoundedCard from './hypercard';
 import HearMeThumbnail from './hearmethumbnail.png'
 import MapMeThumbnail from './mapmethumbnail.png'
 import React from 'react'
+import { isMobile } from 'react-device-detect';
 import './App.css';
 
 function App() {
   document.title = "Karmel's Portfolio"
-
   return (
     <div>
       <div className='Header'>
-        <h1 style={{marginLeft: '4vw', paddingTop: '10vh', fontFamily: 'EB Garamond, serif',fontSize: '5em'}}>Joannes Karmel Gandahusada</h1>
-        <h3 style={{marginLeft: '6vw', marginTop: '-5vh', paddingBottom: '2vh', fontFamily: 'EB Garamond, serif',fontSize: '1em'}}>Data Scientist/Fullstack Developer/AI Engineer</h3>
+        <h1 style={{marginLeft: '4vw', paddingTop: '100px', fontFamily: 'EB Garamond, serif',fontSize: isMobile ? '2em' : '5em'}}>Joannes Karmel Gandahusada</h1>
+        <h3 style={{marginLeft: '6vw', paddingBottom: '40px', fontFamily: 'EB Garamond, serif',fontSize: isMobile? '1em' : '1.5em'}}>Data Scientist/Fullstack Developer/AI Engineer</h3>
       </div>
       <div className='Background-theme'>
-        <p style={{marginLeft: '4vw', paddingTop: '3vh'}}>Since 2019, I have been dedicated to studying and advancing my skills in AI and data science. My journey has been marked by a commitment to continuous learning, and exploring the latest advancements in these fields. In 2021, I started working professionally and have loved it ever since, successfully leveraging my expertise to real-world applications.</p>
+        <p style={{marginLeft: '4vw', paddingTop: '50px'}}>Since 2019, I have been dedicated to studying and advancing my skills in AI and data science. My journey has been marked by a commitment to continuous learning, and exploring the latest advancements in these fields. In 2021, I started working professionally and have loved it ever since, successfully leveraging my expertise to real-world applications.</p>
         <p style={{marginLeft: '4vw'}}>Click on the links below to see the my academic and passion projects that I have worked on during university and my free time.</p>
         <div className="Thumbnail-collection">
           <RoundedCard
